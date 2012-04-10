b724bf6ed8d61200e893c1c32b44c45c226cb70f
Kevin.Lin93 <Kevin.Lin93@4193ed10-ec51-d7a5-d4af-6629140c91e2>
2012-04-10 18:52:05 +0000

    Resolved Conflict in Math.java.

--- src/Math.java
+++ src/Math.java
@@ -1,5 +1,73 @@
+import java.util.*;
 
 public class Math {
+	
+	/**
+	 * High-level evaluation of a mathematical expression.
+	 * @param expression - String array of expression components (Must be in postfix!)
+	 * @return
+	 */
+	public String calculate(String[] expression) {
+		
+		//Result is the final expression as a string.
+		String result = null;
+		
+		//Gets length of our String[]
+		int length = expression.length;
+		
+		//Initializes our "Stack"
+		LinkedList<String> operationStack = new LinkedList<String>();
+		
+		//Iterates through each element in our string array.
+		for(int i = 0; i < length; i++) {
+			//Push item [i] into the stack.
+			operationStack.addFirst(expression[i]);
+			
+			if (isOperator(operationStack.peek())) {
+				//Pop the operator from the stack.
+				String thisElement = operationStack.remove();
+				
+				//Java can't use switch() with strings, so... :/
+				if(thisElement.equals("+")) {
+					//Pushes result of add() into stack, while popping two previous elements.
+					operationStack.addFirst(add(operationStack.remove(), operationStack.remove()));
+				}
+				if(thisElement.equals("-")) {
+					
+				}
+				if(thisElement.equals("*")) {
+					
+				}
+				if(thisElement.equals("/")) {
+					
+				}
+				if(thisElement.equals("^")) {
+					
+				}
+				if(thisElement.equals("sqrt:")) {
+					
+				}
+			}
+		}
+		
+		return result;
+		
+	}
+	
+	/**
+	 * Checks if the inputted string is an operator (i.e. +,-,*,/,^,sqrt:)
+	 * @param input
+	 */
+	public static boolean isOperator(String input) {
+		
+		String[] operators = {"+" ,"-" ,"*" ,"/" ,"^" ,", ", "sqrt:"};
+		
+		for(int i = 0; i < operators.length; i++) {
+			if (input.equals(operators[i])) return true;
+		}
+		return false;
+		
+	}
 	
 	private String add(String arg1, String arg2){
 		// need to determine what strings are, then pass to more specific
