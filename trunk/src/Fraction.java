02adedbcc584357b03d799831faa70a10c7db07a
Kevin.Lin93 <Kevin.Lin93@4193ed10-ec51-d7a5-d4af-6629140c91e2>
2012-04-11 21:49:31 +0000

    Added this.simplify() to all getters and constructors, so that a Fraction will always be in simplest form.

--- trunk/src/Fraction.java
+++ trunk/src/Fraction.java
@@ -3,7 +3,6 @@ public class Fraction {
 	private Irrational Numerator;
 	private Irrational Denominator;
 	
-	
 	public Fraction (String expression){
 		String split[] = expression.split("/");
 		if (split.length == 2){
@@ -29,19 +28,23 @@ public class Fraction {
 		//case when irrational is entered (e,pi)
 		//case when sqrt or ^ is used?
 		//else?
+		this.simplify();
 	}
 	
 	public Fraction (String numerator, String denominator){
 		Numerator = new Irrational(numerator);
 		Denominator = new Irrational(denominator);
+		this.simplify();
 	}
 	
 	public void setNumerator(String val){
 		Numerator = new Irrational(val);
+		this.simplify();
 	}
 	
 	public void setDenominator(String val){
 		Denominator = new Irrational(val);
+		this.simplify();
 	}
 	
 	public String getNumerator(){
