a562df501ce904aff388ca979492daf66c791557
ilovemintbreeze@gmail.com <ilovemintbreeze@gmail.com@4193ed10-ec51-d7a5-d4af-6629140c91e2>
2012-04-11 22:51:29 +0000

    get history, set history, add history

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Calculator 
 {
+	private final int maxHistoryItem = 3;
 	private int currentHistoryItem;
 	private List<HistoryItem> history;
 	
@@ -12,4 +13,26 @@ public class Calculator
 		history = new ArrayList<HistoryItem>();
 	}
 
+	
+	public void addHistoryItem(HistoryItem input)
+	{
+		
+		if(history.size() < maxHistoryItem)
+			history.add(input);
+		else if(history.size() >= maxHistoryItem)
+		{
+			history.remove(0);
+			history.add(input);
+		}
+	}
+	
+	public List<HistoryItem> getHistory()
+	{
+		return history;
+	}
+	
+	public void setCurrentHistoryItem(int choiceNum)
+	{
+		currentHistoryItem = choiceNum;
+	}
 }
