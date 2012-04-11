fb8398215564972bb812ce742b3899dc9b2fe296
ilovemintbreeze@gmail.com <ilovemintbreeze@gmail.com@4193ed10-ec51-d7a5-d4af-6629140c91e2>
2012-04-11 21:35:43 +0000

    made calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Calculator 
+{
+	private int currentHistoryItem;
+	private List<HistoryItem> history;
+	
+	public Calculator()
+	{
+		history = new ArrayList<HistoryItem>();
+	}
+
+}
