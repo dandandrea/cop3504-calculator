25ab46997af2b2121830acd3b0a22841f4120045
millsthegeneral@gmail.com <millsthegeneral@gmail.com@4193ed10-ec51-d7a5-d4af-6629140c91e2>
2012-04-12 21:22:16 +0000

    Updates to constructor, still not finished

--- trunk/src/Irrational.java
+++ trunk/src/Irrational.java
@@ -4,56 +4,56 @@ import java.util.List;
 
 public class Irrational {
 	// Worst case format => 3 * sqrt:2 * pi^3 * e^3, modeled as Num*Sqr*Pi^Pi_e*E^e
-	private String Num; //Integer value
+	private int Num; //Integer value
 	private String Sqr; //Value x in sqrt:x
-	private String Pi_e; //Exponent of pi
-	private String E_e; //Exponent of e
+	private int Pie; //Exponent of pi
+	private int Ee; //Exponent of e
 	
-//*********************everything below this point is bullshit****************************
-	
-	//Constructor handles strings of format A*x+B^e, or any variation
-	//Fractional exponents represented as .e, ie sqrt: would be e = .2
+	//Constructor to handle a string input
 	public Irrational (String expression){
-		if (testForm1(expression) == true)
+		if (testForm1(expression) == true){ // initial check if string is an integer
 			return;
-		else if (testForm2(expression) == true)
-			return;
-		else if (testForm3(expression) == true)
-			return;
-		else if (testForm4(expression) == true)
-			return;
-		else if (testForm5(expression) == true)
-			return;
-		else if (testForm6(expression) == true)
-			return;
-		else if (testForm7(expression) == true)
-			return;
-		else if (testForm8(expression) == true)
-			return;
-		else {
-			testForm9(expression);
+		}
+		String[] full = expression.split("\\*");
+		if (full.length > 3){ //3 instances of '*', must be worst case format
+			testForm2(full);
 			return;
 		}
 	}
 	
-	private boolean testForm2(String expression){ // Form: A*x (symbol, pi or e)
-		
+	private void testForm2(String[] full){ // Form: Num*Sqr*Pi^Pi_e*E^e
+		Num = Integer.parseInt(full[0]);   // assumes for sqrt:x, x contains no '*' symbol
+		Sqr = full[1];
+		String[] split = full[2].split("\\^");
+		Pie = 1;
+		if (split.length == 2){
+			Pie = Integer.parseInt(split[1]);
+		}
+		String[] split2 = full[3].split("\\^");
+		Ee = 1;
+		if (split2.length == 2){
+			Ee = Integer.parseInt(split2[1]);
+		}
+		//probably works...
 	}
 	
-	private boolean testForm1(String expression){ // Form: A (integer)
+	private boolean testForm1(String expression){ // Form: Num (integer)
 		if (SymbolicMath.isInteger(expression) == true){
-			Coeff = expression;
+			Num = Integer.parseInt(expression);
+			Sqr = "1";
+			Pie = 0;
+			Ee = 0;
 			return true;
 		}
 		return false;
 	}
 	
 	
-	public Irrational (String coeff, String constant, String symbol, String exp){
-		Coeff = coeff;
-		Const = constant;
-		Symbol = symbol;
-		Exp = exp;
+	public Irrational (int num, String sqr, int pie, int ee){
+		Num = num;
+		Sqr = sqr;
+		Pie = pie;
+		Ee = ee;
 	}
 	
 		/**
@@ -105,86 +105,7 @@ public class Irrational {
 	}
 	
 	public String canFactor(){
-		String s = null;
-		if ((Const == null) && (Symbol == null) && (Exp == null))
-			return Coeff;
 		
-		else if (Const == null){ //A*x can factor out A, A*x^e can factor out A^e
-			if (Exp == null){ //Factor out the A in A*x
-				s = Coeff;
-			}
-			else{ //Factor out A^e in A*x^e
-				String[] a = {Coeff,Exp,"^"};
-				s = SymbolicMath.calculate(a);
-			}
-		}
-		else if (Exp == null){ //Checks if format A*x+B can factor out an integer
-			int multiple = 1;
-			int coeff = Integer.parseInt(Coeff);
-			int constant = Integer.parseInt(Const);
-			
-			if ((coeff%constant) == 0) //initial check to see if A/B is an integer, if so B is greatest factor
-				return Integer.toString(constant);
-			if ((constant%coeff) == 0) //initial check to see if B/A is an integer, if so A is greatest factor
-				return Integer.toString(coeff);
-				
-			if (coeff > constant){ // pick which is lower, Coeff or Const, and divide by two to start at highest possible factor
-				multiple = constant/2;
-				for(int i = multiple; i != 0; i--){
-					if ((coeff%i == 0) && (constant%i == 0)){ //if integer i divides both A and B without a remainder
-						if (i == 1)
-							return null; //if greatest common factor found is 1, it returns no common factor
-						return Integer.toString(i); //else greatest common factor found is returned
-					}
-				}
-			}
-			else {
-				multiple = coeff/2;
-				for(int i = multiple; i != 0; i--){
-					if ((coeff%i == 0) && (constant%i == 0)){ //if integer i divides both A and B without a remainder
-						if (i == 1)
-							return null; //if greatest common factor found is 1, it returns no common factor
-						return Integer.toString(i); //else greatest common factor found is returned
-					}
-				}
-			}
-		}
-		else {
-			s = null;
-		}
-		return s;
-	}
-	
-	public String toString(){
-		String toString;
-		if ((Const == null) && (Symbol == null) && (Exp == null)){ // format "A", used for integers
-			toString = Coeff;
-		}
-		else if ((Coeff == null) && (Const == null) && (Exp == null)){ // format "x", used for "pi" and "e"
-			toString = Symbol;
-		}
-		else if ((Const == null) && (Exp == null)){ // format "A*x"
-			toString = Coeff + "*" + Symbol;
-		}
-		else if ((Coeff == null) && (Exp == null)){ // format "x+B"
-			toString = Symbol + "+" + Const;
-		}
-		else if (Exp == null){ // format "A*x+B"
-			toString = Coeff + "*" + Symbol + "+" + Const;
-		}
-		else if ((Coeff == null) && (Const == null)){ // format "x^e"
-			toString = Symbol + "^" + Exp;
-		}
-		else if ((Const == null) && (Symbol == null)){ // format "A^e" ***************probably unnecessary, this is equivalent to 2^2
-			toString = Coeff + "^" + Exp;
-		}
-		else if (Const == null){ // format "A*x^e"
-			toString = Coeff + "*" + Symbol + "^" + Exp;
-		}
-		else{ // format "A*x+B^e", any errors should return null in the string
-			toString = Coeff + "*" + Symbol + "+" + Const + "^" + Exp;
-		}
-		return toString;
 	}
 	
 	//testing purposes only
